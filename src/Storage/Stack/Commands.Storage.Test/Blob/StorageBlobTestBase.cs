39fffd2c2a9f412d4df9c75e9c566a1fcd92d7e0
Bala Ganapathy <bganapa@microsoft.com>
2017-11-14 12:38:49 -0800

    Storage tests - 2

--- src/Storage/Stack/Commands.Storage.Test/Blob/StorageBlobTestBase.cs
+++ src/Storage/Stack/Commands.Storage.Test/Blob/StorageBlobTestBase.cs
@@ -15,6 +15,7 @@
 using System;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.WindowsAzure.Commands.Common;
 using Microsoft.WindowsAzure.Commands.Common.Test.Mocks;
 using Microsoft.WindowsAzure.Commands.Storage.Test.Service;
 using Microsoft.WindowsAzure.Storage.Blob;
